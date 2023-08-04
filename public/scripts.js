15adca1bffc0d7fc1469d63a6d400a33f4462838
Laura-Barrera <laura.barrera14@uptc.edu.co>
2023-08-03 19:52:57 -0500

    Parcial completo

--- public/scripts.js
+++ public/scripts.js
@@ -1,57 +1,85 @@
-// Traer datos ejemplo
-function loadAuthors(){
+function loadDishes(){
     return new Promise((resolve, reject) =>{
-        // Aca se hace el fetch para acceder a la API
-        // Fetch es como una promesa
-        fetch('http://localhost:4000/authors')
-        // convertir en formato json y pasar a la siguiente linea
+        fetch('https://api-dishes.vercel.app/')
         .then(resp => resp.json())
         .then(resp => resolve(resp))
         .catch(err => reject(err))
     })
 }
 
-//Visualizar datos Ejemplo
-const data = () =>{
-    const id = document.getElementById('idAuthor').value
-    const name = document.getElementById('nameAuthor').value
-    const birthday = document.getElementById('dateAuthor').value
+function limpiar(){
+    location.reload()
+}
+
+const newDish = () =>{
+    const id = document.getElementById('idDish').value
+    const name = document.getElementById('name').value
+    const calories = document.getElementById('calories').value
+    const isVegetarian = document.getElementById('isVegetarian').value
+    const value = document.getElementById('value').value
+    const comments = document.getElementById('comments').value
     
-    const datos ={"id":`${id}`,"name":`${name}`,"birthday":`${birthday}`}
+    const datos ={"idDish":`${id}`,"name":`${name}`,"calories":`${calories}`,"isVegetarian":`${isVegetarian}`,"value":`${value}`,"comments":`${comments}`}
     return JSON.stringify(datos)
 }
 
-//Encontrar por un ID especifico
-function findId() {
-    const URL = ""
-    fetch(URL,{
-        method:"GET",
-        body: data()
-    })
-    .then(resp => {
-
-    })
-    .catch(err => alert(`error ${err}`))
-}
-
-// Envio datos formulario Ejemplo
-document.getElementById('botonEnvioAutor').addEventListener('click',()=>{
-    const URL = "http://localhost:4000/authors"
+document.getElementById('botonEnvioPlato').addEventListener('click',()=>{
+    const URL = "https://api-dishes.vercel.app/"
 
     fetch(URL,{
         method:"POST",
         headers:{
             "Content-Type":"application/json"
         },
-        body: data()
+        body: newDish()
     })
-    .then(resp => resp.json())
+    .then(resp => {
+        if(resp.status = 208){
+            alert('Ya existe un plato con el mismo id')
+            throw new Error('Plato duplicado');
+        } else{
+            resp.json()
+        }
+    })
     .then(resp => {
         if(resp.state){
+            console.log(resp)
             alert('ok')
+            location.reload()
         } else{
+            // Falta validar los estados
+            console.log(resp)
             alert('no')
         }
     })
     .catch(err => alert(`error ${err}`))
 })
+
+function search() {
+    const idObj = document.getElementById('idPlato').value;
+    return new Promise((resolve, reject) => {
+        fetch(`https://api-dishes.vercel.app/${idObj}`)
+            .then(resp => resp.json())
+            .then(resp => resolve(resp))
+            .catch(err => reject(err));
+    })
+    .then(res => {
+        console.log(res.data._id); 
+        const objid = document.getElementById("ObjS")
+        objid.append(document.createTextNode(`${res.data._id}`))
+        const ids= document.getElementById("idS")
+        ids.append(document.createTextNode(`${res.data.idDish}`))
+        const names = document.getElementById("nameS")
+        names.append(document.createTextNode(`${res.data.name}`))
+        const calorieS = document.getElementById("caloriesS")
+        calorieS.append(document.createTextNode(`${res.data.calories}`))
+        const veget= document.getElementById("vegetarianS")
+        veget.append(document.createTextNode(`${res.data.isVegetarian}`))
+        const valuesS = document.getElementById("valueS")
+        valuesS.append(document.createTextNode(`${res.data.value}`))
+        const commt = document.getElementById("commentS")
+        commt.append(document.createTextNode(`${res.data.comments}`))
+    })
+    .catch(err => alert(`error ${err} dish no encontrado`))
+}
+
